5d3335cd7bc4888f97393ee226550a922303a259
sumitworld <sumitsam465@gmail.com>
2024-01-19 10:01:53 -0700

    matrix demo code

---
+++ MatrixVisualizationDemo/AppConstants/AppConstants.swift
@@ -0,0 +1,8 @@
+//
+//  AppConstants.swift
+//  MatrixVisualizationDemo
+//
+//  Created by Vivek Parmar on 2024-01-19.
+//
+
+import Foundation
---
+++ MatrixVisualizationDemo/View/MatrixBoxCell.swift
@@ -0,0 +1,12 @@
+//
+//  MatrixBoxCell.swift
+//  MatrixVisualizationDemo
+//
+//  Created by Vivek Parmar on 2024-01-19.
+//
+
+import UIKit
+
+class MatrixBoxCell: UICollectionViewCell {
+    
+}
